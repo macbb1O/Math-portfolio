c0b8de4733013eff0df030b09e6b2dbf4e81f044
macbb1O <macbb1318@gmail.com>
2025-12-17 16:27:38 -0500

    Update generateQuestion.ts

--- client/src/lib/generateQuestion.ts
+++ client/src/lib/generateQuestion.ts
@@ -1,1 +1,15 @@
+// generateQuestion.ts
+import type { GeneratedQuestion } from "./generatedQuestion";
+import type { QuestionType } from "./questionTypes";
+import { generateLinearEquation } from "./generators/linearEquation";
+
+export function generateQuestion(type: QuestionType): GeneratedQuestion {
+  switch (type) {
+    case "linear-equation":
+      return generateLinearEquation();
+
+    default:
+      throw new Error(`No generator found for question type: ${type}`);
+  }
+}
 
