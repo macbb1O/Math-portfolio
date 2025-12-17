9c42e34101f4f54a170defbb372d6c3201c3ad62
macbb1O <macbb1318@gmail.com>
2025-12-17 16:18:51 -0500

    Update questionType.ts

--- client/src/lib/questionType.ts
+++ client/src/lib/questionType.ts
@@ -1,1 +1,11 @@
+// questionTypes.ts
+
+export type QuestionType =
+  | "linear-equation"
+  | "two-step-equation"
+  | "graphing-lines"
+  | "pythagorean-theorem"
+  | "sohcahtoa"
+  | "sine-law"
+  | "cosine-law";
 
